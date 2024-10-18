21b75a79d7ab1e4cc2b9f367d7977ceac65d1b8d
oleh <phd.chornyi@gmail.com>
2024-10-18 12:59:51 +0300

    4th: deals

--- public/main.js
+++ public/main.js
@@ -45,3 +45,8 @@ ScrollReveal().reveal(".header__content p", {
     ...scrollRevealOption,
     delay: 1500,
 });
+
+ScrollReveal().reveal(".deals__card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
