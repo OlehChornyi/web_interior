8c338ce5fb0ccbabb73c689ee9cb1e35021266bb
oleh <phd.chornyi@gmail.com>
2024-10-18 12:40:29 +0300

    3rd: header

--- public/main.js
+++ public/main.js
@@ -16,6 +16,32 @@ navLinks.addEventListener("click", (e) => {
 
 const navSearch = document.getElementById("nav-search");
 
-navSearch.addEventListener("click", (e)=> {
+navSearch.addEventListener("click", (e) => {
     navSearch.classList.toggle("open");
+});
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header__content div", {
+    duration: 1000,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1500,
 });
